b7cdb0df8463e832eef41024d6340c1e42430b49
kevinmurphy678 <kevinmurphy678@gmail.com>
2017-01-21 00:18:06 -0500

    Initial Project

---
+++ desktop/src/com/hac/kua/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hac.kua.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hac.kua.HacKua;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HacKua(), config);
+	}
+}
